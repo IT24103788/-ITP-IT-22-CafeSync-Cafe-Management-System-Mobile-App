4c4e74fed666fa583d70504a961ac4d5bb1372ab
IT24103788 <it24103788@my.sliit.lk>
2026-05-03 18:05:06 +0530

    update: route all requests to new backend deployment URL

--- mobile/src/api/client.js
+++ mobile/src/api/client.js
@@ -7,11 +7,15 @@ import { Platform } from 'react-native';
 // Get the host IP address (works for both emulator and physical devices)
 const helperIp = Constants.expoConfig?.hostUri?.split(`:`)[0];
 
-let BASE_URL = 'http://10.0.2.2:5000/api'; // Default for Android Emulator
-if (Platform.OS === 'web') {
-  BASE_URL = 'http://localhost:5000/api';
-} else if (helperIp) {
-  BASE_URL = `http://${helperIp}:5000/api`;
+let BASE_URL = process.env.EXPO_PUBLIC_API_URL;
+
+if (!BASE_URL) {
+  BASE_URL = 'http://10.0.2.2:5000/api'; // Default for Android Emulator
+  if (Platform.OS === 'web') {
+    BASE_URL = 'http://localhost:5000/api';
+  } else if (helperIp) {
+    BASE_URL = `http://${helperIp}:5000/api`;
+  }
 }
 const apiClient = axios.create({
   baseURL: BASE_URL,
